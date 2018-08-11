91630f889cbdfe9d99de4bf6d5a48f17772d55f1
Rob Nash <robscode@icloud.com>
2018-08-11 12:34:19 +0100

    Add Echo module and EchoTests module

---
+++ Echo/Echo.h
@@ -0,0 +1,19 @@
+//
+//  Echo.h
+//  Echo
+//
+//  Created by Robert Nash on 11/08/2018.
+//  Copyright © 2018 Elaborapp Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Echo.
+FOUNDATION_EXPORT double EchoVersionNumber;
+
+//! Project version string for Echo.
+FOUNDATION_EXPORT const unsigned char EchoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Echo/PublicHeader.h>
+
+
